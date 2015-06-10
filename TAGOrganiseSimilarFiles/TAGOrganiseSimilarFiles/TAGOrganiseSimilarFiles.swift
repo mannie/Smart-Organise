fe977f7c40e5eab05a59d7302b326c341a36a07b
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-10 16:52:26 -0700

    [swiftify] Updated TAGOrganiseSimilarFiles to be a Swift project

---
+++ TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles.swift
@@ -0,0 +1,17 @@
+//
+//  TAGOrganiseSimilarFiles.swift
+//  TAGOrganiseSimilarFiles
+//
+//  Created by etagarira on 10/06/2015.
+//
+//
+
+import Automator
+
+class TAGOrganiseSimilarFiles: AMBundleAction {
+
+    override func runWithInput(input: AnyObject!, error: NSErrorPointer) -> AnyObject! {
+        return input
+    }
+    
+}
