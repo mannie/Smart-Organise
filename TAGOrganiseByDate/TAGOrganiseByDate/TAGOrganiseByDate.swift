6d5cf3d855ec66adcb380f7b0e2bfa9673515afa
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-10 16:51:49 -0700

    [swiftify] Updated TAGOrganiseByDate to be a Swift project

---
+++ TAGOrganiseByDate/TAGOrganiseByDate/TAGOrganiseByDate.swift
@@ -0,0 +1,17 @@
+//
+//  TAGOrganiseByDate.swift
+//  TAGOrganiseByDate
+//
+//  Created by etagarira on 10/06/2015.
+//
+//
+
+import Automator
+
+class TAGOrganiseByDate: AMBundleAction {
+
+    override func runWithInput(input: AnyObject!, error: NSErrorPointer) -> AnyObject! {
+        return input
+    }
+    
+}
