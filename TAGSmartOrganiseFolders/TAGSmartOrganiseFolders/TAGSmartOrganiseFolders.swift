5a375ca085417058832a17daaa5c2988506000a4
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-10 16:52:52 -0700

    [swiftify] Updated TAGSmartOrganiseFolders to be a Swift project

---
+++ TAGSmartOrganiseFolders/TAGSmartOrganiseFolders/TAGSmartOrganiseFolders.swift
@@ -0,0 +1,13 @@
+//
+//  TAGSmartOrganiseFolders.swift
+//  TAGSmartOrganiseFolders
+//
+//  Created by etagarira on 10/06/2015.
+//
+//
+
+import Automator
+
+class TAGSmartOrganiseFolders: AMBundleAction {
+
+}
