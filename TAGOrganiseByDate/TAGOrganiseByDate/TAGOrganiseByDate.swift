8ce2f0e000e7d508c8a0ae61ebe6d3b5dbec6ff0
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-11 14:05:18 -0700

    [swiftify] Implemented Organise By Date in Swift using the helpers from the toolbox

--- TAGOrganiseByDate/TAGOrganiseByDate/TAGOrganiseByDate.swift
+++ TAGOrganiseByDate/TAGOrganiseByDate/TAGOrganiseByDate.swift
@@ -7,11 +7,37 @@
 //
 
 import Automator
+import TAGToolbox
 
 class TAGOrganiseByDate: AMBundleAction {
 
-    override func runWithInput(input: AnyObject!, error: NSErrorPointer) -> AnyObject! {
-        return input
+    override func runWithInput(input: AnyObject!, error: NSErrorPointer) -> AnyObject! {        
+        let inputFilePaths = Set<String>(input as! Array<String>)
+        var outputFilePaths = Set<String>()
+        
+        let dateString = String(date: NSDate(), format: "yyyy-MM-dd")
+        
+        let fileManager = NSFileManager.defaultManager()
+        
+        for sourcePath in inputFilePaths {
+            if !fileManager.fileExistsAtPath(sourcePath) {
+                continue
+            }
+            
+            let parentDirectory = sourcePath.stringByDeletingLastPathComponent
+            var destinationPath = parentDirectory.stringByAppendingPathComponent(dateString)
+
+            fileManager.createDirectoryAtPath(destinationPath, withIntermediateDirectories: true, attributes: nil, error: nil)
+            
+            destinationPath = destinationPath.stringByAppendingPathComponent(sourcePath.lastPathComponent)
+            destinationPath = fileManager.collisionSafePath(destinationPath)
+            
+            if fileManager.moveItemAtPath(sourcePath, toPath: destinationPath, error: nil) {
+                outputFilePaths.insert(destinationPath)
+            }
+        }
+        
+        return outputFilePaths
     }
     
 }
