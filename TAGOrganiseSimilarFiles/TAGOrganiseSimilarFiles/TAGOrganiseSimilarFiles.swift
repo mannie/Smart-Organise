cd642b4c032684ef105a0a5f9d8af36ee9a8e68f
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-12 16:56:37 -0700

    [swiftify] Implemented of Organise Similar Files in Swift

--- TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles.swift
+++ TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles/TAGOrganiseSimilarFiles.swift
@@ -7,11 +7,48 @@
 //
 
 import Automator
+import TAGToolbox
+
+extension NSFileManager {
+    
+    func organiseFilesAtPath(path: String, withExtension fileExt: String) -> Set<String> {
+        var outputFilePaths = Set<String>()
+
+        changeCurrentDirectoryPath(path)
+        createDirectoryAtPath(fileExt, withIntermediateDirectories: true, attributes: nil, error: nil)
+        
+        for sourcePath in subpathsAtPath(".", withExtension: fileExt) {
+            let destinationPath = collisionSafePath(fileExt.stringByAppendingPathComponent(sourcePath))
+
+            if moveItemAtPath(sourcePath, toPath: destinationPath, error: nil) {
+                outputFilePaths.insert(path.stringByAppendingPathComponent(destinationPath))
+            }
+        }
+        
+        return outputFilePaths
+    }
+    
+}
 
 class TAGOrganiseSimilarFiles: AMBundleAction {
 
     override func runWithInput(input: AnyObject!, error: NSErrorPointer) -> AnyObject! {
-        return input
+        let inputFilePaths = Set<String>(input as! Array<String>)
+        var outputFilePaths = Set<String>()
+
+        for sourcePath in inputFilePaths {
+            let fileExt = sourcePath.pathExtension
+            if fileExt.isEmpty {
+                outputFilePaths.insert(sourcePath)
+                continue
+            }
+            
+            let parentDirectory = sourcePath.stringByDeletingLastPathComponent
+            let organisedFiles = NSFileManager.defaultManager().organiseFilesAtPath(parentDirectory, withExtension: fileExt)
+            outputFilePaths.unionInPlace(organisedFiles)
+        }
+        
+        return outputFilePaths
     }
     
 }
