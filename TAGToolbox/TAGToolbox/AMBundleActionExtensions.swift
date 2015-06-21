70cddf148c1e96d0c245608c878d5c96219359a7
Mannie Tagarira <etagarira@yammer-inc.com>
2015-06-20 23:35:29 -0700

    [swiftify] Updated deliverUserNotification to assign the default project icon

--- TAGToolbox/TAGToolbox/AMBundleActionExtensions.swift
+++ TAGToolbox/TAGToolbox/AMBundleActionExtensions.swift
@@ -11,14 +11,20 @@ import Automator
 public extension AMBundleAction {
     
     func deliverUserNotification(filePaths: Array<String>) {
+        let notification = NSUserNotification()
+        notification.title = name
+        
         let fileExts = filePaths.map { $0.pathExtension }
         let uniqueFileExts = Set(fileExts).subtract(Set(arrayLiteral: ""))
         
-        let joinedFileExts = (Array(uniqueFileExts) as NSArray).componentsJoinedByString(", ")
-        
-        let notification = NSUserNotification()
-        notification.title = name
-        notification.informativeText = "\(filePaths.count) item" + (filePaths.count == 1 ? "" : "s") + " | \(joinedFileExts)"
+        var informativeText = "\(filePaths.count) item" + (filePaths.count == 1 ? "" : "s")
+        if uniqueFileExts.count > 0 {
+            informativeText += " | " + (Array(uniqueFileExts) as NSArray).componentsJoinedByString(", ")
+        }
+        notification.informativeText = informativeText
+
+        let bundle = NSBundle.allBundles().filter { $0.bundleIdentifier!!.hasPrefix("com.etagarira") }.first as! NSBundle
+        notification.contentImage = NSImage(contentsOfFile: bundle.pathForImageResource("icon")!)
         
         NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
     }
