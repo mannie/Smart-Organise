da7df68918fd5c40b359ad880949f66bf8a1106c
Mannie Tagarira <etagarira@yammer-inc.com>
2015-11-01 20:40:09 +0000

    Sign the installer using Dev App identity, not Dev Installer identity

--- Installer/sign-packages.sh
+++ Installer/sign-packages.sh
@@ -7,9 +7,18 @@ APPLICATION_IDENTITY="Developer ID Application: E Tagarira (3AMCJ5B2Q4)"
 
 SUB_PKG_DESTINATION="Smart-Organise.mpkg/Contents/Packages"
 
+echo ""
 productsign --sign "$INSTALLER_IDENTITY" Smart-Organise-Automator.pkg "$SUB_PKG_DESTINATION/Smart-Organise-Automator.pkg"
+
+echo ""
 productsign --sign "$INSTALLER_IDENTITY" Smart-Organise-Services.pkg "$SUB_PKG_DESTINATION/Smart-Organise-Services.pkg"
+
+echo ""
 productsign --sign "$INSTALLER_IDENTITY" Smart-Organise-FolderActions.pkg "$SUB_PKG_DESTINATION/Smart-Organise-FolderActions.pkg"
 
-productsign --sign "$APPLICATION_IDENTITY" Smart-Organise.mpkg ../Smart-Organise.mpkg
-codesign -d -vvvv ../Smart-Organise.mpkg
+echo ""
+codesign -s "$APPLICATION_IDENTITY" Smart-Organise.mpkg
+codesign -d -vvvv Smart-Organise.mpkg
+
+echo ""
+cp -R Smart-Organise.mpkg ../Smart-Organise.mpkg
